afacd7e4145f169f8e80bca34c9ffa0a0348d560
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2023-11-23 10:39:19 -0600

    Update dash.lua

--- nvim/dash.lua
+++ nvim/dash.lua
@@ -13,3 +13,8 @@
         "    ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆       ",
         "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
         "                                   "
+
+-- also windows download
+-- downloaded directly from the repo
+-- clone config repos like this
+-- git clone https://github.com/config/repo $HOME\AppData\Local\nvim --depth 1
