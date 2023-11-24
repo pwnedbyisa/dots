c07d17b69ab1a6327d018b57309b2ce2eb4ab06a
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2023-11-23 19:06:04 -0600

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,6 +166,13 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+  -- Add dashboard
+  'goolord/alpha-nvim',
+  config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+  end
+  },
  
 }, {})
 
@@ -536,5 +543,40 @@ cmp.setup {
   },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+--[[dash]]
+
+local status_ok, alpha = pcall(require, 'alpha')
+if not status_ok then
+    return
+end
+
+local function header()
+    return require('hydra')['random']
+end
+
+local dashboard = require('alpha.themes.dashboard')
+dashboard.section.header.val = header()
+
+-- Menu
+dashboard.section.buttons.val = {
+  dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+  dashboard.button('f', '  Find file', ':NvimTreeOpen<CR>'),
+  dashboard.button('s', '  Settings', ':e $MYVIMRC<CR>'),
+  dashboard.button('u', '  Update plugins', ':Lazy update<CR>'),
+  dashboard.button('q', '  Quit', ':qa<CR>'),
+}
+
+
+-- Footer
+local function footer()
+  local version = vim.version()
+  local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
+  local datetime = os.date('%Y/%m/%d %H:%M:%S')
+
+  return print_version .. ' - ' .. datetime
+end
+
+
+dashboard.section.footer.val = footer()
+
+alpha.setup(dashboard.config)
