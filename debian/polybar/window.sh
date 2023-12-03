bb893b062d0ac704eb2e60f039fe11bf3ca72ebc
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2023-12-02 23:18:29 -0600

    Create window.sh

---
+++ debian/polybar/window.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# directly plagarized from my kali config which was plagarized from https://github.com/aroma1994/polybar-windows/blob/master/polybar-windows.sh 
+# i'm still too lazy to fork so
+
+active_window=$(xprop -root _NET_ACTIVE_WINDOW|cut -d ' ' -f 5|sed -e 's/../0&/2')
+current_display=$(wmctrl -d|grep "*"|awk '{print $1}')
+
+current_windows=$(wmctrl -lx|awk -v current_display="$current_display" -v active_window="$active_window" '
+
+        {if ($2==current_display) {
+
+                if ($1==active_window) {
+
+                        $3="#"$3;
+
+                        }
+
+                split($3,window_title,".")
+
+                print "%{A1: wmctrl -ia "$1" & disown:}"window_title[1]"%{A}"
+
+                }
+
+        }')
+
+echo $current_windows
