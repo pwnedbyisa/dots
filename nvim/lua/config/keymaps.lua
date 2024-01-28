50764417b72b12155cd8ea9d89428d3c29ccdd68
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-01-27 18:09:58 -0600

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,25 +1,29 @@
 local keymap = vim.keymap
 
 -- plugins
-keymap.set("n", "<leader>lu", ":Lazy update<CR>")
+keymap.set("n", "<leader>lu", ":Lazy update<CR>", { silent = true} )
 
 -- directory nav
 keymap.set("n","<leader>fo", ":NvimTreeOpen<CR>", { noremap = true, silent = true})
 keymap.set("n","<leader>fc", ":NvimTreeClose<CR>", { noremap = true, silent = true})
 
 -- modes
-keymap.set("n", "<leader>tt", ":TransparentToggle<CR>")
-keymap.set("n", "<leader>z", ":ZenMode<CR>")
+keymap.set("n", "<leader>tt", ":TransparentToggle<CR>", {silent = true })
+keymap.set("n", "<leader>z", ":ZenMode<CR>", {silent = true})
 
 -- multi pane nav
 -- figure out later
 
 -- window splitting
-keymap.set("n", "<leader>sv", ":vsplit<CR>")
-keymap.set("n", "<leader>sh", ":split<CR>")
+keymap.set("n", "<leader>sv", ":vsplit<CR>", {silent = true})
+keymap.set("n", "<leader>sh", ":split<CR>", {silent = true})
 
 -- indents
-keymap.set("v", "<", "<gv")
-keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv", {silent = true})
+keymap.set("v", ">", ">gv", {silent = true})
+
+-- undo
+keymap.set("n", "<leader>u", ":undo<CR>", {silent = true}) 
+keymap.set("n", "<leader>r", ":redo<CR>", {silent = true})
 
 -- comments - gcc and gbc
