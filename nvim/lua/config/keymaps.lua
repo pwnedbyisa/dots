6628c54a3b429d4e88a2edbbcc9bd108ad636258
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-01-31 15:49:22 -0600

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,11 +1,15 @@
 local keymap = vim.keymap
 
+-- movement remapping <3
+-- :set whichwrap+=h,l
+
 -- plugins
 keymap.set("n", "<leader>lu", ":Lazy update<CR>", { silent = true} )
 
 -- directory nav
 keymap.set("n","<leader>fo", ":NvimTreeOpen<CR>", { noremap = true, silent = true})
 keymap.set("n","<leader>fc", ":NvimTreeClose<CR>", { noremap = true, silent = true})
+keymap.set("n", "<leader>oi", ":Oil --float<CR>", { noremap = true, silent = true }) 
 
 -- modes
 keymap.set("n", "<leader>tt", ":TransparentToggle<CR>", {silent = true })
@@ -23,7 +27,7 @@ keymap.set("v", "<", "<gv", {silent = true})
 keymap.set("v", ">", ">gv", {silent = true})
 
 -- undo
-keymap.set("n", "<leader>u", ":undo<CR>", {silent = true}) 
+keymap.set("n", "<leader>u", ":undo<CR>", {silent = true})
 keymap.set("n", "<leader>r", ":redo<CR>", {silent = true})
 
 -- quit
