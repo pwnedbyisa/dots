b0e79405752ac36ce6c613af669ef8ff08039444
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-03-01 16:38:57 -0600

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,15 @@
 if status is-interactive
+    # theme
     fish_config theme choose 'Mono Lace'
+    
+    # full directories instead of first letter
     set fish_prompt_pwd_dir_length 0
+    
+    # disable default greeting
+    set fish_greeting
 
+    # basically nim prompt but without any of the features/ colors
     function fish_prompt
-
     echo -n '┬─ ['
 
     if functions -q fish_is_root_user; and fish_is_root_user
