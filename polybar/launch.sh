8508d67a0fed2f1f614b3cff88d8a754fa8acae1
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-03-02 09:25:12 -0600

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# plagarized directly from the polybar wiki
+
+# Terminate already running bar instances
+# ipc enabled
+polybar-msg cmd quit
+# Otherwise
+# killall -q polybar
+
+# Launch bar
+echo "---" | tee -a /tmp/polybar.log
+polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Bars launched..."
