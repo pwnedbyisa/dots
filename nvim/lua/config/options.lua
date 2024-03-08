656bab140e96a58a77fc01d0f426acbef4260d6f
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-03-08 10:40:55 -0600

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,7 +6,7 @@ opt.shiftwidth = 4
 opt.softtabstop = 2
 opt.expandtab = true
 opt.smartindent = true
-opt.wrap = false
+opt.wrap = true
 
 -- search
 opt.incsearch = true
@@ -18,7 +18,6 @@ opt.hlsearch = false
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
-opt.colorcolumn = '100'
 opt.signcolumn = "yes"
 opt.cmdheight = 1
 opt.scrolloff = 10
@@ -39,5 +38,4 @@ opt.iskeyword:append("-")
 opt.mouse:append('a')
 opt.clipboard:append("unnamedplus")
 opt.modifiable = true
--- opt.guicursor =
 opt.encoding = "UTF-8"
