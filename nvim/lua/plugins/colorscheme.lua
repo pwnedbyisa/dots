a6e56d55fea1a8c15384fea3503c3a83943e388a
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2024-05-28 13:55:01 -0500

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 -- love this one
 --return {
---	"rebelot/kanagawa.nvim",
---	lazy = false,
---	priority = 777,
---	config = function()
+--      "rebelot/kanagawa.nvim",
+--      lazy = false,
+--      priority = 777,
+--      config = function()
 --    -- or kanagawa-lotus
---	  vim.cmd('colorscheme kanagawa-dragon')
---	end
+--        vim.cmd('colorscheme kanagawa-dragon')
+--      end
 --}
 
 -- basic but good
---return {
---    "ellisonleao/gruvbox.nvim",
---    lazy = false,
---    priority = 777,
---    config = function()
---        vim.cmd('colorscheme gruvbox')
---    end
---}
+return {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 777,
+    config = function()
+        vim.cmd('colorscheme gruvbox')
+    end
+}
 
 -- pinkkk
 -- return {
@@ -30,12 +30,12 @@
 -- }
 
 
--- matches desktop
-return {
-  "xero/miasma.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme miasma")
-  end,
-}
+-- matches desktop but has some issues w transparency
+-- return {
+--  "xero/miasma.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    vim.cmd("colorscheme miasma")
+--  end,
+--}
